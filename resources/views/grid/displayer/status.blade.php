0e0a0196ef880b9ed43fb60f0a1682bf14828a58
Mikhail Bredikhin <mikha.dev@gmail.com>
2021-12-30 06:49:49 +0100

    status

--- resources/views/grid/displayer/status.blade.php
+++ resources/views/grid/displayer/status.blade.php
@@ -1,5 +1,15 @@
-@if($status)
-    <i title='{{$titleOn}}' class='fa fa-circle text-success'>
-@else
-    <i title='{{$titleOff}}' class='fa fa-circle text-danger'>
-@endif
+<input disabled class="grid-column-switch" data-size="small" {{ $checked }} type="checkbox" data-color="{{ $color }}"/>
+
+<script require="@switchery">
+    var swt = $('.grid-column-switch'),
+        that;
+    function initSwitchery() {
+        swt.parent().find('.switchery').remove();
+        swt.each(function () {
+            that = $(this);
+            new Switchery(that[0], that.data())
+        })
+    }
+    initSwitchery();
+
+</script>
--- src/Grid/Displayers/StatusDisplay.php
+++ src/Grid/Displayers/StatusDisplay.php
@@ -7,14 +7,26 @@ use Dcat\Admin\Admin;
 class StatusDisplay extends AbstractDisplayer
 {
 
-    public function display(string $titleOn = 'On', string $titleOff = 'Off')
+    public function color($color)
+    {
+        $this->color = Admin::color()->get($color);
+    }
+
+    public function display(string $color = '')
     {
         $column = $this->column->getName();
-        $status = $this->value;
+        if ($color instanceof \Closure) {
+            $color->call($this->row, $this);
+        } else {
+            $this->color($color);
+        }
+
+        $color = $this->color ?: Admin::color()->primary();
+        $checked = $this->value ? 'checked' : '';
 
         return Admin::view(
             'admin::grid.displayer.status',
-            compact('column', 'status', 'titleOn','titleOff')
+            compact('column', 'checked', 'color')
         );
     }
 }
