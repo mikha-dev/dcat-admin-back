5bcd5aadffab4efa7d197a19b89e9b639817d4cf
mikha-dev <mikha.dev@gmail.com>
2023-12-20 17:56:50 +0300

    icon

--- src/DcatIcon.php
+++ src/DcatIcon.php
@@ -40,6 +40,7 @@ enum DcatIcon: string {
     case MEDAL = 'medal';
     case CONTACT_US = 'phone-alt';
     case SHARE_ALT = 'share-alt';
+    case HAND_DOWN = 'hand-o-down';
 
 	public function _(bool $fullTag = FALSE, ?string $title = null) {
 		return self::format($this, $fullTag, $title);
